1fc6190f0e58e86528cdc37c26a672cd48a364e5
Emily Daitch <emily.daitch@gmail.com>
2023-05-21 16:32:54 -0400

    secondary example thread

--- Thread.c
+++ Thread.c
@@ -9,15 +9,31 @@ void Thread (void const *argument);                             // thread functi
 osThreadId tid_Thread;                                          // thread id
 osThreadDef (Thread, osPriorityNormal, 1, 0);                   // thread object
 
-int Init_Thread (void) {
+void thread1 (void const *argument); //function prototype for thread1
+osThreadId thread1_id; //thread handle
+osThreadDef(thread1, osPriorityNormal, 1, 0); //thread definition structure
+
+int Init_Threads (void) {
 
   tid_Thread = osThreadCreate (osThread(Thread), NULL);
   if (!tid_Thread) return(-1);
   
+	uint32_t startupParameter = 0x23; // For now, as example, parameter does nothing
+	thread1_id = osThreadCreate(osThread(thread1), &startupParameter); // Note: tutorial mistake fixed here.
+  if (!thread1_id) return(-1);
+
   return(0);
 }
 
 void Thread (void const *argument) {
+
+  while (1) {
+    ; // Insert thread code here...
+    osThreadYield ();                                           // suspend thread
+  }
+}
+
+void thread1 (void const *argument) {
 
   while (1) {
     ; // Insert thread code here...
--- main.c
+++ main.c
@@ -9,13 +9,27 @@
 /*
  * main: initialize and start the system
  */
+extern int Init_Threads (void);
+osThreadId main_id;
+
 int main (void) {
+	
+	main_id = osThreadGetId ();
+	
   osKernelInitialize ();                    // initialize CMSIS-RTOS
 
   // initialize peripherals here
-
+	Init_Threads ();
   // create 'thread' functions that start executing,
   // example: tid_name = osThreadCreate (osThread(name), NULL);
 
   osKernelStart ();                         // start thread execution 
+	
+	// We have the option to continue to use the main thread
+	//while (1) {
+		
+	//}
+	// Otherwise, execution will end as normal.
+	// Tutorial suggests using osTerminate, but it does not appear to be standard for CMSIS RTOSv1. Will investigate further.
+	// osTerminate(main_id);
 }

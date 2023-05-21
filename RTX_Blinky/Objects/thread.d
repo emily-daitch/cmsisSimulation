2f37769de856fab4473be1d947fd97978f6d63f3
Emily Daitch <emily.daitch@gmail.com>
2023-05-20 22:01:26 -0400

    objects and listings

---
+++ RTX_Blinky/Objects/thread.d
@@ -0,0 +1,4 @@
+./objects/thread.o: Thread.c \
+  C:\Users\emily\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS\RTX\INC\cmsis_os.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
